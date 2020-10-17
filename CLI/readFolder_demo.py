427206bcf780e95ea74fe4e3233649728fc5c23d
j <wuchin@oregonstate.edu>
2020-10-16 22:08:44 -0700

    Add the function turns all comments into one string

--- CLI/readFolder_demo.py
+++ CLI/readFolder_demo.py
@@ -1,7 +1,38 @@
 import argparse
 import json
 
+def Main():
+        path = input('Enter your folder path:')
+        myFB = Facebook(path)
+        all_Comments = myFB.comments()
+        print(comments_str(all_Comments))
+
+def comments_str(comments):
+    """Pick the content from comments dictionary and combine all of them into one string.
+
+    Args:
+      An array of comments. All the info are in dictioanry format.
+
+    Returns:
+      A string.
+    """
+    comments_str = ""
+    for aComments in comments:
+        datas = aComments["data"]
+        for data in datas:
+            str = data["comment"]["comment"]
+            comments_str = comments_str + " " + str
+    return comments_str
+
 class Facebook:
+    """A Facebook instance provides data including comments, apps.
+
+    Args:
+      The path of the folder user downloaded from Facebook.
+
+    Returns:
+      All the returns are arrays of dictionaries.
+    """
     def __init__(self, folder):
         self.folder = folder
     
@@ -22,14 +53,6 @@ class Facebook:
               return d["installed_apps"]
         except:
             print("read apps' json fail") 
-
-
-def Main():
-        path = input('Enter your folder path:')
-        myFB = Facebook(path)
-        for app in myFB.apps():
-            print(app['name'])
-
 
 
 if __name__ == "__main__":
