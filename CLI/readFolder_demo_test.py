5f4d02ce5d131f0b74f6c7770c21f8db11de0336
jcfwu <72423396+jcfwu@users.noreply.github.com>
2020-10-23 14:14:28 -0700

    Pytest about combining comments into one string

---
+++ CLI/readFolder_demo_test.py
@@ -0,0 +1,11 @@
+import pytest
+from readFolder_demo import comments_str
+
+def test_empty():
+  assert comments_str([]) == ""
+
+def test_invalid_data_1():
+  assert comments_str([{'timestamp':1601406498}]) == ""
+
+def test_invalid_data_2():
+  assert comments_str([{'timestamp':1601406498, 'data':[{'comment':{'comment':"Hello"}}]}]) == " Hello"
